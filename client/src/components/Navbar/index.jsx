3d0e6e893e1ee378a6b3ca7af82934f18c8a6b7f
Svetlana Kollau <svetlana.kollau@gmail.com>
2020-01-12 22:27:29 +0200

    add auth0

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
 import SubNavbarThemes from '../SubNavbarThemes'
+import { useAuth0 } from '../../react-auth0-spa'
+
 export default function Navbar() {
+  const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0()
+
+  const logoutWithRedirect = () =>
+    logout({
+      returnTo: window.location.origin
+  })
+
   return (
     <Fade left>
     <div className={style.sidebar}>
@@ -39,6 +48,27 @@ export default function Navbar() {
         <Link to='/about'>
         <div className={style.nav__li}>About us</div>
         </Link>
+
+        {isAuthenticated
+          ? (
+            <div 
+              className={style.nav__li}
+              onClick={() => logoutWithRedirect({})}
+            >
+              Log Out
+            </div>
+          )
+          : (
+            <div 
+              className={style.nav__li}
+              onClick={() => loginWithRedirect({})}
+            >
+              Log In
+            </div>
+          )
+          
+        }
+       
         </Fade>
       </div>
     </div>
--- client/src/components/QuestionAdd/index.jsx
+++ client/src/components/QuestionAdd/index.jsx
@@ -4,26 +4,26 @@ export default function QuestionAdd(props) {
   
   return (
     <div className={style.container}>
-    <div class="md-form input-group">
-          <input type="text" class="form-control" placeholder="Your question" aria-label="Recipient's username with two button addons"
+    <div className="md-form input-group">
+          <input type="text" className="form-control" placeholder="Your question" aria-label="Recipient's username with two button addons"
             aria-describedby="MaterialButton-addon4"></input>
             
          
     </div>
     <div className={style.answers}>
 
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <div class="input-group-text">
+    <div className="input-group mb-3">
+      <div className="input-group-prepend">
+        <div className="input-group-text">
           <input type="checkbox" aria-label="Checkbox for following text input"></input>
         </div>
       </div>
-      <input type="text" class="form-control" placeholder="Your answer option" aria-label="Text input with checkbox"></input>
-      <div class="input-group-append">
+      <input type="text" className="form-control" placeholder="Your answer option" aria-label="Text input with checkbox"></input>
+      <div className="input-group-append">
     <button className={style.del} type="button" id="MaterialButton-addon2">X</button>
   </div>
     </div>
-        <button class="btn btn-md btn-info m-0 px-3" type="button">+ option</button>
+        <button className="btn btn-md btn-info m-0 px-3" type="button">+ option</button>
 </div>
     
     </div>
