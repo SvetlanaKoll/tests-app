54a68f810f8d757ebc84162ec1a3be483e86690d
Svetlana Kollau <svetlana.kollau@gmail.com>
2020-01-23 10:55:35 +0200

    add render to topics page

--- client/src/components/ThemeItem/index.jsx
+++ client/src/components/ThemeItem/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import style from './index.module.css'
 import { Link } from 'react-router-dom'
 
-export default function ThemeItem(props) {
+export default function ThemeItem (props) {
   return (
     <div className={style.box}>
-      <div className={style.box__title}>Pregnancy</div>
-      <div className={style.box__desc}>Here you could discover a lot of different signs of pregnancy, check your condition, find correct answers on your questions</div>
-      <div className={style.box__q}>Quantity of tests: 4</div>
-    <Link to={`/tests`}><button className={style.box__btn}>Go to this theme!</button></Link>
+      <div className={style.box__title}>{props.title}</div>
+      <div className={style.box__desc}>{props.description}</div>
+      <div className={style.box__q}>Quantity of tests: ?</div>
+      <Link to={'/tests'}><button className={style.box__btn}>Go to this theme!</button></Link>
     </div>
   )
 }
--- client/src/pages/Topics/index.jsx
+++ client/src/pages/Topics/index.jsx
@@ -1,22 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './index.module.css'
 import { withRouter } from 'react-router-dom'
 import ThemeItem from '../../components/ThemeItem'
 import FirstBlock from '../../components/FirstBlock'
 import Search from '../../components/Search'
 import lamp from '../../static/lamp.webp'
-function Topics () {
-  const [topics, setThemes] = useState([])
-  const [themesToDisplay, setThemesToDisplay] = useState([])
+import doFetch from '../../utils/doFetch'
+import { useAuth0 } from '../../react-auth0-spa'
 
-  const filterThemes = searchString => setThemesToDisplay(searchString ? topics.filter(theme => theme.title.includes(searchString) || theme.description.includes(searchString)) : topics)
+function Topics () {
+  const [topics, setTopics] = useState([])
+  const [topicsToDisplay, setTopicsToDisplay] = useState([])
+  const { token } = useAuth0()
+  useEffect(() => {
+    (async () => {
+      if (token) {
+        try {
+          const data = await doFetch(token, 'GET', 'topics')
+
+          console.log(data)
+
+          if (data.success) {
+            console.log(data.topics)
+            setTopics(data.topics)
+            setTopicsToDisplay(data.topics)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+    })()
+  }, [token])
+
+  const filterThemes = searchString => setTopicsToDisplay(searchString ? topics.filter(theme => theme.title.includes(searchString) || theme.description.includes(searchString)) : topics)
   return (
     <div className={style.container}>
       <FirstBlock image={lamp} title='Topics' color='#fff'/>
       <Search onChange={filterThemes}/>
       <div className={style.topics}>
-        {/* smth to render. map( item => ...) */}
-        <ThemeItem name='English'/>
+        {topicsToDisplay.map(({ _id, title, description }) => (
+          <ThemeItem key={_id} _id={_id} title={title} description={description}/>
+        ))}
+
       </div>
     </div>
   )
